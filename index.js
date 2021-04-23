8ad4064d9eb8eb21494f49adaad2ee9de7f366a1
KoonPaT <40105170+KoonPaT@users.noreply.github.com>
2021-04-23 13:37:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,163 @@
+const prompt = require('prompt-sync')();
+const chalk = require('chalk');
+var setTitle = require('console-title');
+setTitle('Facebook Hacking By . . .');
+console.clear()
+
+
+//By P H A T
+
+
+console.log(chalk.red(`
+	╔═╗╔═╗╔═╗╔═╗╔╗ ╔═╗╔═╗╦╔═
+	╠╣ ╠═╣║  ║╣ ╠╩╗║ ║║ ║╠╩╗
+	╚  ╩ ╩╚═╝╚═╝╚═╝╚═╝╚═╝╩ ╩
+	╦ ╦╔═╗╔═╗╦╔═╦╔╗╔╔═╗     
+	╠═╣╠═╣║  ╠╩╗║║║║║ ╦     	
+	╩ ╩╩ ╩╚═╝╩ ╩╩╝╚╝╚═╝ 	By . . . 
+	`));
+
+let idfb = prompt(chalk.white('Enter Id Facebook: '));
+
+if (idfb.length !== 15) {
+
+	console.log(chalk.red("Id Facebook Not Correct"))
+
+}else{
+
+	main()
+
+}
+
+function main() {
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Please Wait For Hacking!'));
+
+	}, 5000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Please Wait For Hacking!'));
+
+	}, 10000);
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 19 %'))
+
+	}, 20000);
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 19 %'))
+
+	}, 50000);
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 51 %'))
+
+	}, 90000);
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 84 %'))
+
+	}, 300000);
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 99 %'))
+
+	}, 600000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 99 %'))
+
+	}, 900000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 99 %'))
+
+	}, 1200000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 99 %'))
+
+	}, 1400000);
+
+
+	main2()
+
+}
+
+function main2() {
+
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 85 %'))
+
+	}, 300000);
+
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 89 %'))
+
+	}, 600000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 90 %'))
+
+	}, 900000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 95 %'))
+
+	}, 1200000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.yellow('Status : 1 %'))
+
+	}, 1400000);
+
+
+	setTimeout(() => {
+
+		console.log(chalk.red('Status : ERROR!! | Please Try Again!!'))
+
+		var a = prompt(' ');
+
+	}, 17000000);
+
+
+}
